08e3bc95da1f8917f00f983b97dafbc10cdce1ab
j-256 <james@jklein.dev>
2024-06-24 00:37:58 +0000

    Simplify controller and add note on caching

--- cartridge/controllers/Static.js
+++ cartridge/controllers/Static.js
@@ -1,12 +1,13 @@
 'use strict';
 const server = require('server');
 
+// NOTE: Cache time will be determined by the configured static cache TTL due to the static resource being remote-included; setting it here will do nothing
 server.get('File', function(req, res, next) {
     const Template = require('dw/util/Template')
     const HashMap = require('dw/util/HashMap');
     const pdict = new HashMap();
     pdict.put('path', req.querystring.path);
-    response.setContentType(req.querystring['content-type'] || req.querystring.contentType);
+    response.setContentType(req.querystring['content-type']);
     const template = new Template('static');
     response.writer.print(template.render(pdict).text);
 });
